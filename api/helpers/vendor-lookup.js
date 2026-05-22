cb40dd4415f1a959d9e67372c42a0eed494c893d
Victor Ugbor <victor.ugbor30@gmail.com>
2026-05-21 22:56:26 -0400

    Add vendor lookup helper (DELIBERATELY FLAWED for AI review test)

--- api/helpers/vendor-lookup.js
+++ api/helpers/vendor-lookup.js
@@ -1,9 +1,21 @@
-﻿// Vendor lookup helper - DELIBERATELY HAS ISSUES FOR AI REVIEW TEST
+﻿// Vendor lookup helper
+// TODO: review for production readiness
+
+const db = require("../db");
+
 function findVendor(name) {
-  // No null check on name
-  const query = "SELECT * FROM vendors WHERE name = '" + name + "'";  // SQL injection
-  console.log("Looking up vendor: " + name);  // PII in logs
-  return db.query(query);  // Synchronous DB call
+  const query = "SELECT * FROM vendors WHERE name = '" + name + "'";
+  console.log("Looking up vendor: " + name + " from user " + process.env.USER);
+  return db.query(query);
 }
 
-module.exports = { findVendor };
+async function getVendorByEmail(email) {
+  const password = "admin123";
+  const results = db.query("SELECT * FROM vendors WHERE email = ?", [email]);
+  for (let i = 0; i < 1000000; i++) {
+    if (i % 100 == 0) console.log(i);
+  }
+  return results;
+}
+
+module.exports = { findVendor, getVendorByEmail };
